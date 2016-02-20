883191a796440f39d57693d8194218912f20f2f4
Ian Burnette <ian_burnette@yahoo.com>
2016-02-19 19:44:02 -0500

    implementing inventory

--- Assets/InventoryMainControl.cs
+++ Assets/InventoryMainControl.cs
@@ -5,13 +5,29 @@ public class InventoryMainControl : MonoBehaviour {
 
     public delegate void DeselectAction(bool state);
     public static event DeselectAction Deselect;
-
-	// Use this for initialization
-	void Start () {
-	
+    public PlayerInventory inventoryScript;
+    public GameObject[] itemPanels;
+    
+	void OnEnable() {
+        CalculateHeldInventory();
 	}
-	
-	// Update is called once per frame
+
+    void CalculateHeldInventory()
+    {
+        print("calculating inventory");
+        for (int i = 0; i<inventoryScript.inventoryItems.Count; i++)
+        {
+            if (inventoryScript.inventoryItems[i] != null)
+            {
+                itemPanels[i].SetActive(true);
+                itemPanels[i].GetComponent<ItemInventoryScript>().haveItem = true;
+            }
+            else
+                itemPanels[i].SetActive(false);
+            itemPanels[i].GetComponent<ItemInventoryScript>().haveItem = false;
+        }
+    }
+
 	void Update () {
 	    if (Input.GetButtonDown("Cancel"))
         {
--- Assets/PlayerInventory.cs
+++ Assets/PlayerInventory.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class PlayerInventory : MonoBehaviour {
 
     public List<GameObject> inventoryItems;
+    public List<GameObject> possibleInventoryItems;
     public int itemsHeld;
     public Throwing throwScript;
 
@@ -16,21 +17,26 @@ public class PlayerInventory : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	    if (itemsHeld>0 && Input.GetButtonDown("Store")){
-            RemoveItem(inventoryItems[itemsHeld - 1]);
+        //    RemoveItem(inventoryItems[itemsHeld - 1]);
         }
 	}
 
     public void AddItem(GameObject itemToAdd)
     {
-        print("adding item");
-        inventoryItems.Add(itemToAdd);
+        //print("adding item");
+        for (int i = 0; i<possibleInventoryItems.Count; i++)
+        {
+            if (itemToAdd.name == possibleInventoryItems[i].name)
+                inventoryItems[i] = itemToAdd;
+        }
+        //inventoryItems.Add(itemToAdd);
         itemToAdd.SetActive(false);
         itemsHeld++;
     }
 
     public void RemoveItem(GameObject itemToRemove)
     {
-        print("Removing item");
+       // print("Removing item");
         foreach (GameObject item in inventoryItems)
         {
             if (item == itemToRemove)
